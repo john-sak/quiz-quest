20470273a3c39d3b87fe1ecd784f013e04792bbe
Giannis Sakarellos <giannis.sak@icloud.com>
2023-07-17 16:55:52 +0300

    initial commit

---
+++ QuizQuest/QuizQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizQuestApp.swift
+//  QuizQuest
+//
+//  Created by Ioannis Sakarellos on 17/7/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
